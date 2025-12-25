19c155a67d5737b08cfb913341c8004dc6bdfb6f
pongo1231 <pongo@gopong.dev>
2025-12-25 18:41:19 +0100

    mvm_sandbox2: init

---
+++ configs/mvm_sandbox2/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "\n" >> ../mvm_sandbox2.log
+while true; do ./source_logger -game tf +maxplayers 32 +map $(../../randommap.sh) +sv_pure -1 +ip 0.0.0.0 -port 27022 +tv_enable 1 +tv_port 26022 2>&1 | grep --line-buffered -v Setting\ sv_visiblemaxplayers\ to\ ..\ for\ MvM | tee -a ../mvm_sandbox2.log; done
