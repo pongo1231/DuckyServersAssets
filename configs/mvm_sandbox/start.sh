cf78dbfbf8e5751a991f3776aaa10c6d779f7428
pongo1231 <pongo@gopong.dev>
2025-12-25 14:03:34 +0100

    adjust sourcetv ports

--- configs/mvm_sandbox/start.sh
+++ configs/mvm_sandbox/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "\n" >> ../mvm_sandbox.log
-while true; do ./source_logger -game tf +maxplayers 32 +map $(../../randommap.sh) +sv_pure -1 +ip 0.0.0.0 -port 27016 +tv_enable 1 +tv_port 27021 2>&1 | grep --line-buffered -v Setting\ sv_visiblemaxplayers\ to\ ..\ for\ MvM | tee -a ../mvm_sandbox.log; done
+while true; do ./source_logger -game tf +maxplayers 32 +map $(../../randommap.sh) +sv_pure -1 +ip 0.0.0.0 -port 27016 +tv_enable 1 +tv_port 26016 2>&1 | grep --line-buffered -v Setting\ sv_visiblemaxplayers\ to\ ..\ for\ MvM | tee -a ../mvm_sandbox.log; done
--- configs/mvm_vanilla/start.sh
+++ configs/mvm_vanilla/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "\n" >> ../mvm_vanilla.log
-while true; do ./source_logger -game tf +maxplayers 32 +map $(../../randommap.sh) +sv_pure 1 +ip 0.0.0.0 -port 27015 +tv_enable 1 +tv_port 27024 2>&1 | tee -a ../mvm_vanilla.log; done
+while true; do ./source_logger -game tf +maxplayers 32 +map $(../../randommap.sh) +sv_pure 1 +ip 0.0.0.0 -port 27015 +tv_enable 1 +tv_port 26015 2>&1 | tee -a ../mvm_vanilla.log; done
